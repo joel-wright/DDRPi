5f09f6cbcc38410f7f7f949db6416f585c8ccc92
Joel Wright <joel.wright@gmail.com>
2012-11-21 10:36:49 +0100

    Comment updates

--- Layout.py
+++ Layout.py
@@ -1,13 +1,29 @@
 __author__ = 'Joel Wright'
 
-class DisplayLayout(gtk.Window):
-    def __init__(self, config, image_surface):
+import gtk
+from DDRPi import Plugin
+
+class DisplayLayout(gtk.Window, Plugin):
+    def __init__(self):
         super(DisplayLayout, self).__init__()
 
-        # This is an example of an end user module - need to make sure we can get the main image surface and config
-        # to write to them both...
+    def config(self, config, image_surface):
+        """
+        This is an example of an end user module - need to make sure we can get the main image surface and config
+        to write to them both...
+        """
         self.ddrpi_config = config
         self.ddrpi_surface = image_surface
+
+    def start(self):
+        """
+        Start writing to the surface
+        """
+
+    def stop(self):
+        """
+        Stop writing to the surface, de-register callbacks
+        """
 
     def on_timer(self):
         # add this
