ee8cf2d2227ada0bf414336250fbe5c7197cfd0d
Joel Wright <joel.wright@gmail.com>
2013-02-06 23:34:37 +0100

    Finish pong plugin

--- plugins/pong.py
+++ plugins/pong.py
@@ -14,6 +14,19 @@ class PongPlugin(DDRPiPlugin):
 		1: 'player2'
 	}
 	
+	__numbers__ = {
+		0: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x,y+3),(x,y+4),(x+1,y),(x+1,y+4),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		1: lambda (x,y): [(x,y+1),(x,y+4),(x+1,y),(x+1,y+1),(x+1,y+2),(x+1,y+3),(x+1,y+4),(x+2,y+4)],
+		2: lambda (x,y): [(x,y),(x,y+2),(x,y+3),(x,y+4),(x+1,y),(x+1,y+2),(x+1,y+4),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+4)],
+		3: lambda (x,y): [(x,y),(x,y+4),(x+1,y),(x+1,y+2),(x+1,y+4),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		4: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x+1,y+2),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		5: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x,y+4),(x+1,y),(x+1,y+2),(x+1,y+4),(x+2,y),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		6: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x,y+3),(x,y+4),(x+1,y),(x+1,y+2),(x+1,y+4),(x+2,y),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		7: lambda (x,y): [(x,y),(x+1,y),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		8: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x,y+3),(x,y+4),(x+1,y),(x+1,y+2),(x+1,y+4),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)],
+		9: lambda (x,y): [(x,y),(x,y+1),(x,y+2),(x+1,y),(x+1,y+2),(x+2,y),(x+2,y+1),(x+2,y+2),(x+2,y+3),(x+2,y+4)]
+	}
+	
 	def configure(self, config, image_surface):
 		"""
 		Called to configure the plugin before we start it.
@@ -37,16 +50,19 @@ class PongPlugin(DDRPiPlugin):
 		Stop the plugin if necessary - e.g. stop writing to the dance surface.
 		"""
 		self.game_state['state'] = "STOPPED"
-		pygame.time.set_timer(USEREVENT+0,0)
-		pygame.time.set_timer(USEREVENT+1,0)
-		pygame.time.set_timer(USEREVENT+2,0)
-		pygame.time.set_timer(USEREVENT+3,0)
+		self._disable_move_events()
 		
 	def pause(self):
 		"""
 		Pauses the plugin - e.g. saves a game state when we enter menu mode.
 		"""
 		self.game_state['state'] = "PAUSED"
+		self._disable_move_events()
+		
+	def _disable_move_events(self):
+		"""
+		Disable recurring movement events
+		"""
 		pygame.time.set_timer(USEREVENT+0,0)
 		pygame.time.set_timer(USEREVENT+1,0)
 		pygame.time.set_timer(USEREVENT+2,0)
@@ -56,7 +72,7 @@ class PongPlugin(DDRPiPlugin):
 		"""
 		Resumes the plugin from a paused state.
 		"""
-		if self.__state__ == "STOPPED":
+		if self.game_state['state'] == "STOPPED":
 			self._draw_state()
 		else: # restart repeating events 
 			self.game_state['state'] = "RUNNING"
@@ -133,6 +149,8 @@ class PongPlugin(DDRPiPlugin):
 					pygame.time.set_timer(USEREVENT+event_number,speed)
 					self._move_bat(player,self.game_state[player]['direction'])
 				elif event_number == 2: # USEREVENT+2 = x-axis ball motion
+					speed = self.game_state['ball_x_speed']
+					pygame.time.set_timer(USEREVENT+event_number,speed)
 					logging.debug("PongPlugin: Handling x-axis ball motion")
 					delta = self.game_state["ball_x_direction"]
 					in_play = self._move_ball((delta, 0))
@@ -178,6 +196,14 @@ class PongPlugin(DDRPiPlugin):
 				if button == 0:
 					self._reset()
 					self.start()
+		elif self.game_state['state'] == "BETWEEN_POINTS":
+			if pygame.event.event_name(event.type) == "UserEvent":
+				event_number = event.type - 24
+				if event_number == 4: # Event 4 is the restart event after a point
+					pygame.time.set_timer(USEREVENT+4,0)
+					self.resume()
+				else:
+					logging.debug("PongPlugin: Unknown user event")
 		else:
 			logging.debug("PongPlugin: Need to handle state: " % self.__state__)
 		
@@ -216,6 +242,10 @@ class PongPlugin(DDRPiPlugin):
 		p2by = self.game_state['player2']['position']
 		self.ddrpi_surface.draw_tuple_box((w-2,p2by),(w-2,p2by+2),white)
 		
+		st = self.game_state['state']
+		if not st == "RUNNING":
+			self._draw_score()
+		
 		# blit to the floor
 		self.ddrpi_surface.blit()
 		
@@ -235,11 +265,10 @@ class PongPlugin(DDRPiPlugin):
 			},
 			'button_repeat_speed': 100,
 			'initial_repeat_delay': 200,
-			'next_serve': 0,
 			'ball_x_direction': 1,
-			'ball_x_speed': 300, # I expect this to remain constant
-			'ball_y_direction': 1,
-			'ball_y_speed': 300, # Updated when the ball hits the bat, refreshed every y-move userevent
+			'ball_x_speed': 150, # I expect this to remain constant
+			'ball_y_direction': [1,-1][random.randint(0,1)],
+			'ball_y_speed': 150, # Updated when the ball hits the bat, refreshed every y-move userevent
 			'ball_position': (2,h/2-1),
 			'state': "RUNNING",
 			'bat_size': 3
@@ -265,10 +294,11 @@ class PongPlugin(DDRPiPlugin):
 		(dx,dy) = delta
 		(cpx,cpy) = self.game_state['ball_position']
 		new_pos = (npx,npy) = (cpx+dx,cpy+dy)
+		w = self.ddrpi_surface.width
 
-		if self._hits_bat(new_pos):
-			# TODO: Calculate new y speed
-			# TODO: Update game_state
+		if self._hits_bat(new_pos) and cpx > 1 and cpx < w - 2:
+			self._update_y_speed(npy)
+			self._update_x_speed()
 			current_direction = self.game_state['ball_x_direction']
 			self.game_state['ball_x_direction'] = -current_direction
 			new_pos_x = (cpx - current_direction,cpy)
@@ -284,7 +314,6 @@ class PongPlugin(DDRPiPlugin):
 			return True
 		else:
 			self.game_state['ball_position'] = new_pos
-			return True
 			# Move the ball
 			
 		w = self.ddrpi_surface.width
@@ -292,6 +321,50 @@ class PongPlugin(DDRPiPlugin):
 			return False
 		else:
 			return True
+
+	def _update_x_speed(self):
+		"""
+		Smoothly update the speed for the ball motion in the x-axis
+		"""
+		speed = self.game_state['ball_x_speed']
+		speed -= 5
+		if not speed < 75:
+			self.game_state['ball_x_speed'] = speed
+			
+	def _update_y_speed(self, y):
+		"""
+		Calculate the new update speed for the ball motion in the y-axis
+		"""
+		w = self.ddrpi_surface.width
+		(bx,by) = self.game_state['ball_position']
+		speed = self.game_state['ball_y_speed']
+		if bx <= 2: # we need to compare the y axis position to p1's bat
+			bat_y = self.game_state['player1']['position']
+			if not by == bat_y + 1: # if we hit the middle then leave as is 
+				direction = self.game_state['ball_y_direction']
+				if by == bat_y + 1 + direction: # Increase speed
+					speed -= random.randint(25,50)
+				else:
+					speed += random.randint(25,50)
+		elif bx >= w-3: # we need to compare the y axis position to p2's bat
+			bat_y = self.game_state['player2']['position']
+			if not by == bat_y + 1: # if we hit the middle then leave as is
+				direction = self.game_state['ball_y_direction']
+				if by == bat_y + 1 + direction: # Increase speed
+					speed -= random.randint(25,50)
+				else:
+					speed += random.randint(25,50)
+		else:
+			logging.debug("PongPlugin: Shouldn't be updating the y speed in the middle of the court")
+			
+		if speed < 30:
+			self.game_state['ball_y_speed'] = speed
+		elif speed > 400:
+			direction = [1,-1][random.randint(0,1)]
+			self.game_state['ball_y_speed'] = speed
+			self.game_state['ball_y_direction'] = direction
+		else:
+			self.game_state['ball_y_speed'] = speed
 			
 	def _hits_bat(self, pos):
 		"""
@@ -331,19 +404,69 @@ class PongPlugin(DDRPiPlugin):
 		"""
 		Handles the event of a player missing the ball
 		"""
-		# TODO: Disable move events
-		# TODO: Update score
-		# TODO: note the player that won the last point
-		# TODO: Display score
-		# TODO:
-		# If point ended the game:
-			# setup event to display message about starting new game
-		# else:
-			# setup event to start next point (1s)
+		self.game_state['state'] = "BETWEEN_POINTS"
+		# Disable move events
+		self._disable_move_events()
+		# Update score
+		(bx,by) = self.game_state['ball_position']
+		w = self.ddrpi_surface.width
+		h = self.ddrpi_surface.height
+		if bx == 0:
+			self.game_state['player2']['score'] += 1
+			self.game_state['ball_position'] = (w-3,h/2-1)
+			self.game_state['ball_x_direction'] = -1 
+		elif bx == w-1:
+			self.game_state['player1']['score'] += 1
+			self.game_state['ball_position'] = (2,h/2-1)
+			self.game_state['ball_x_direction'] = 1
+		self.game_state['player1']['position'] = h/2-2
+		self.game_state['player2']['position'] = h/2-2
+		self.game_state['ball_x_speed'] = 150
+		self.game_state['ball_y_speed'] = 150
+		self.game_state['ball_y_direction'] = [1,-1][random.randint(0,1)]
+		
+		winner = None
+		p1_score = self.game_state['player1']['score']
+		p2_score = self.game_state['player2']['score']
+		if p1_score == 9:
+			winner = 'player1'
+			self.game_state['state'] = "STOPPED"
+		elif p2_score == 9:
+			winner = 'player2'
+			self.game_state['state'] = "STOPPED"
+		else:
+			pygame.time.set_timer(USEREVENT+4,2000)
+			
+		logging.debug("PongPlugin Score: Player 1 (%s) - Player 2 (%s)" % (p1_score, p2_score))
 
-	def _start_new_point():
+	def _draw_score(self):
 		"""
-		Starts a new point...
+		Output the current score onto the game area
 		"""
-		# TODO: reset relevant info (direction, speeds and potitions)
-		# TODO: restart movement timers
+		w = self.ddrpi_surface.width
+		h = self.ddrpi_surface.height
+		
+		p1sx = (w/2-3)/2 + 1
+		p2sx = (w/2-3)/2 + w/2
+		psy = h/2 - 3
+		
+		p1_score = self.game_state['player1']['score']
+		p1_score_pixels = PongPlugin.__numbers__[p1_score]((p1sx,psy))
+		p2_score = self.game_state['player2']['score']
+		p2_score_pixels = PongPlugin.__numbers__[p2_score]((p2sx,psy))
+		
+		white = (255,255,255)
+		red = (255,0,0)
+		
+		for (x,y) in p1_score_pixels:
+			if p2_score == 9:
+				self.ddrpi_surface.draw_tuple_pixel(x,y,red)
+			else:
+				self.ddrpi_surface.draw_tuple_pixel(x,y,white)
+			
+		for (x,y) in p2_score_pixels:
+			if p1_score == 9:
+				self.ddrpi_surface.draw_tuple_pixel(x,y,red)
+			else:
+				self.ddrpi_surface.draw_tuple_pixel(x,y,white)
+
