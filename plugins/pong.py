1580d284485bf2e65fb90199b158dcd4ace41381
Joel Wright <joel.wright@gmail.com>
2013-02-06 18:44:07 +0100

    Better pygame_controller experiment and pong updates

--- plugins/pong.py
+++ plugins/pong.py
@@ -10,8 +10,8 @@ from pygame.locals import *
 class PongPlugin(DDRPiPlugin):
 	# Static map from joypad to player name
 	__player__ = {
-		0: "player1",
-		1: "player2"
+		0: 'player1',
+		1: 'player2'
 	}
 	
 	def configure(self, config, image_surface):
@@ -26,25 +26,44 @@ class PongPlugin(DDRPiPlugin):
 		"""
 		Start the plugin.
 		"""
-		raise NotImplementedError
+		self.game_state['state'] = "RUNNING"
+		x_speed = self.game_state['ball_x_speed']
+		pygame.time.set_timer(USEREVENT+2,x_speed)
+		y_speed = self.game_state['ball_y_speed']
+		pygame.time.set_timer(USEREVENT+3,y_speed)
 
 	def stop(self):
 		"""
 		Stop the plugin if necessary - e.g. stop writing to the dance surface.
 		"""
-		raise NotImplementedError
+		self.game_state['state'] = "STOPPED"
+		pygame.time.set_timer(USEREVENT+0,0)
+		pygame.time.set_timer(USEREVENT+1,0)
+		pygame.time.set_timer(USEREVENT+2,0)
+		pygame.time.set_timer(USEREVENT+3,0)
 		
 	def pause(self):
 		"""
 		Pauses the plugin - e.g. saves a game state when we enter menu mode.
 		"""
-		raise NotImplementedError
+		self.game_state['state'] = "PAUSED"
+		pygame.time.set_timer(USEREVENT+0,0)
+		pygame.time.set_timer(USEREVENT+1,0)
+		pygame.time.set_timer(USEREVENT+2,0)
+		pygame.time.set_timer(USEREVENT+3,0)
 		
 	def resume(self):
 		"""
 		Resumes the plugin from a paused state.
 		"""
-		raise NotImplementedError
+		if self.__state__ == "STOPPED":
+			self._draw_state()
+		else: # restart repeating events 
+			self.game_state['state'] = "RUNNING"
+			x_speed = self.game_state['ball_x_speed']
+			pygame.time.set_timer(USEREVENT+2,x_speed)
+			y_speed = self.game_state['ball_y_speed']
+			pygame.time.set_timer(USEREVENT+3,y_speed)
 		
 	def display_preview(self):
 		"""
@@ -98,23 +117,27 @@ class PongPlugin(DDRPiPlugin):
 				joypad = event.joy
 				player = PongPlugin.__player__[joypad]
 				direction = int(event.value)
-				if direction == 0:
-					pygame.time.set_timer(USEREVENT+joypad,0)
-				else:
-					repeat_speed = self.game_state['initial_repeat_delay']
-					pygame.time.set_timer(USEREVENT+joypad,repeat_speed)
-					self.game_state[player]['direction'] = direction
+				if event.axis in [0,1]: # Ignore extra axes from complicated controllers
+					if direction == 0:
+						pygame.time.set_timer(USEREVENT+joypad,0)
+					else:
+						repeat_speed = self.game_state['initial_repeat_delay']
+						pygame.time.set_timer(USEREVENT+joypad,repeat_speed)
+						self.game_state[player]['direction'] = direction
+						self._move_bat(player,self.game_state[player]['direction'])
 			elif pygame.event.event_name(event.type) == "UserEvent":
 				event_number = event.type - 24
 				if event_number < 2: # Events 0 and 1 are the repeat moves for players
 					player = PongPlugin.__player__[event_number]
 					speed = self.game_state['button_repeat_speed']
 					pygame.time.set_timer(USEREVENT+event_number,speed)
-					self._move(player,self.game_state[player]['direction'])
+					self._move_bat(player,self.game_state[player]['direction'])
 				elif event_number == 2: # USEREVENT+2 = x-axis ball motion
 					logging.debug("PongPlugin: Handling x-axis ball motion")
 					delta = self.game_state["ball_x_direction"]
-					self._move_ball((delta, 0))
+					in_play = self._move_ball((delta, 0))
+					if not in_play:
+						self._player_missed()
 				elif event_number == 3: # USEREVENT+3 = y-axis ball motion
 					logging.debug("PongPlugin: Handling y-axis ball motion")
 					# The current y-direction speed is set when the ball hits a bat
@@ -123,7 +146,7 @@ class PongPlugin(DDRPiPlugin):
 					speed = self.game_state['ball_y_speed']
 					pygame.time.set_timer(USEREVENT+event_number,speed)
 					delta = self.game_state['ball_y_direction']
-					self._move_ball((0, delta))
+					in_play = self._move_ball((0, delta)) # A move in the y-axis cannot put the ball out of play
 				else:
 					logging.debug("PongPlugin: Tried to handle an unknown USEREVENT")
 			elif pygame.event.event_name(event.type) == "JoyButtonDown":
@@ -186,6 +209,13 @@ class PongPlugin(DDRPiPlugin):
 					self.ddrpi_surface.draw_tuple_pixel(w/2-1,y,grey)
 					
 		# Draw the current player bats and position of the ball
+		(bx,by) = self.game_state['ball_position']
+		self.ddrpi_surface.draw_tuple_pixel(bx,by,white)
+		p1by = self.game_state['player1']['position']
+		self.ddrpi_surface.draw_tuple_box((1,p1by),(1,p1by+2),white)
+		p2by = self.game_state['player2']['position']
+		self.ddrpi_surface.draw_tuple_box((w-2,p2by),(w-2,p2by+2),white)
+		
 		# blit to the floor
 		self.ddrpi_surface.blit()
 		
@@ -194,12 +224,12 @@ class PongPlugin(DDRPiPlugin):
 		h = self.ddrpi_surface.height
 		self.game_state = {
 			'player1': {
-				'position': h/2-1,
+				'position': h/2-2,
 				'score': 0,
 				'direction': 0
 			},
 			'player2': {
-				'position': h/2-1,
+				'position': h/2-2,
 				'score': 0,
 				'direction': 0
 			},
@@ -210,7 +240,7 @@ class PongPlugin(DDRPiPlugin):
 			'ball_x_speed': 300, # I expect this to remain constant
 			'ball_y_direction': 1,
 			'ball_y_speed': 300, # Updated when the ball hits the bat, refreshed every y-move userevent
-			'ball_position': (2,h/2),
+			'ball_position': (2,h/2-1),
 			'state': "RUNNING",
 			'bat_size': 3
 		}
@@ -236,16 +266,24 @@ class PongPlugin(DDRPiPlugin):
 		(cpx,cpy) = self.game_state['ball_position']
 		new_pos = (npx,npy) = (cpx+dx,cpy+dy)
 
-		hit_bat = self._hits_bat(new_pos)
-		if hit_bat:
-			# Calculate new y speed
-			# Update game_state
-			# reverse x-direction
+		if self._hits_bat(new_pos):
+			# TODO: Calculate new y speed
+			# TODO: Update game_state
+			current_direction = self.game_state['ball_x_direction']
+			self.game_state['ball_x_direction'] = -current_direction
+			new_pos_x = (cpx - current_direction,cpy)
 			# move the ball
+			self.game_state['ball_position'] = new_pos_x
 			return True
-		elif hit_side:
+		elif self._hits_side(new_pos):
+			current_direction = self.game_state['ball_y_direction']
+			self.game_state['ball_y_direction'] = -current_direction
+			new_pos_y = (cpx,cpy - current_direction)
+			# move the ball
+			self.game_state['ball_position'] = new_pos_y
 			return True
 		else:
+			self.game_state['ball_position'] = new_pos
 			return True
 			# Move the ball
 			
@@ -255,3 +293,57 @@ class PongPlugin(DDRPiPlugin):
 		else:
 			return True
 			
+	def _hits_bat(self, pos):
+		"""
+		Tests whether the positon given is along a player's bat
+		"""
+		(px,py) = pos
+		w = self.ddrpi_surface.width
+		
+		if px == 1: # Player1 bat x-coord
+			bat_pos = self.game_state['player1']['position']
+			if py > bat_pos+2 or py < bat_pos:
+				return False
+			else:
+				return True
+		elif px == w-2: # Player 2 bat x-coord
+			bat_pos = self.game_state['player2']['position']
+			if py > bat_pos+2 or py < bat_pos:
+				return False
+			else:
+				return True
+		else:
+			return False 
+
+	def _hits_side(self, pos):
+		"""
+		Tests whether the positon given is along the side of the playing area
+		"""
+		(px,py) = pos
+		h = self.ddrpi_surface.height
+		
+		if py == 0 or py == h-1:
+			return True
+		else:
+			return False
+			
+	def _player_missed(self):
+		"""
+		Handles the event of a player missing the ball
+		"""
+		# TODO: Disable move events
+		# TODO: Update score
+		# TODO: note the player that won the last point
+		# TODO: Display score
+		# TODO:
+		# If point ended the game:
+			# setup event to display message about starting new game
+		# else:
+			# setup event to start next point (1s)
+
+	def _start_new_point():
+		"""
+		Starts a new point...
+		"""
+		# TODO: reset relevant info (direction, speeds and potitions)
+		# TODO: restart movement timers
